0dd6afd81f891f0fd5ddfe1da7b9801dda0da3d0
SandhiSM <91532714+SandhiSM@users.noreply.github.com>
2024-12-08 14:03:23 +0900

    Solved the stack overflow problem by using a for loop insted of recursion.

--- src/main.rs
+++ src/main.rs
@@ -6,50 +6,30 @@ use std::{
 
 fn main() {
     match env::args().nth(1).unwrap().parse::<u128>() {
-        Ok(n) => {
+        Ok(limit) => {
             {
-                let file = File::create("primes.dat").unwrap();
-                let mut writer = BufWriter::new(file);
-                writer.write_all(b"2\n").unwrap();
+                BufWriter::new(File::create("primes.dat").unwrap())
+                    .write_all(b"2\n")
+                    .unwrap();
             }
-            calc(3, 2, 0, n);
-            return;
+            for dividend in (3..=limit).step_by(2) {
+                let reader = BufReader::new(File::open("primes.dat").unwrap());
+                for divisor in reader.lines() {
+                    let divisor = divisor.unwrap().parse::<u128>().unwrap();
+                    if divisor * divisor > dividend {
+                        BufWriter::new(OpenOptions::new().append(true).open("primes.dat").unwrap())
+                            .write_all(format!("{}\n", dividend).as_bytes())
+                            .unwrap();
+                        break;
+                    } else if dividend % divisor == 0 {
+                        break;
+                    }
+                }
+            }
         }
-        Err(_) => {
-            println!(
-                "Read the README.md file to understand how to use this application and try again."
-            );
-            return;
-        }
+        Err(_) => println!(
+            "Read the README.md file to understand how to use this application and try again."
+        ),
     }
-}
-
-fn calc(dividend: u128, divisor: u128, index: usize, limit: u128) {
-    if divisor * divisor > dividend {
-        write(dividend);
-    } else if dividend % divisor != 0 {
-        return calc(dividend, read(index + 1), index + 1, limit);
-    }
-    if dividend != u128::MAX && dividend + 2 <= limit {
-        return calc(dividend + 2, 2, 0, limit);
-    } else {
-        return;
-    }
-}
-
-fn read(index: usize) -> u128 {
-    BufReader::new(File::open("primes.dat").unwrap())
-        .lines()
-        .into_iter()
-        .nth(index)
-        .unwrap()
-        .unwrap()
-        .parse()
-        .unwrap()
-}
-
-fn write(prime: u128) {
-    BufWriter::new(OpenOptions::new().append(true).open("primes.dat").unwrap())
-        .write_all(format!("{}\n", prime).as_bytes())
-        .unwrap();
+    return;
 }
