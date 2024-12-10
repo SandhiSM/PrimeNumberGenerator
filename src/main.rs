d6afaa653392233479340936530ccab605a2c04f
SandhiSM <91532714+SandhiSM@users.noreply.github.com>
2024-12-11 00:52:52 +0900

    Completely removed the overflow problem.

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,40 @@ use std::{
     env,
     fs::{File, OpenOptions},
     io::{BufRead, BufReader, BufWriter, Write},
+    path::Path,
 };
+
+const SQRT_MAX: u128 = 18446744073709551616;
+const FILE: &str = "primes.dat";
 
 fn main() {
     match env::args().nth(1).unwrap().parse::<u128>() {
         Ok(limit) => {
-            {
-                BufWriter::new(File::create("primes.dat").unwrap())
+            let mut start = 3;
+            if Path::new(FILE).exists() {
+                start = BufReader::new(File::open(FILE).unwrap())
+                    .lines()
+                    .last()
+                    .unwrap()
+                    .unwrap()
+                    .parse::<u128>()
+                    .unwrap()
+                    + 2;
+                if start >= limit {
+                    println!("The file already contains all the prime numbers up to the limit you entered.");
+                    return;
+                }
+            } else {
+                BufWriter::new(File::create(FILE).unwrap())
                     .write_all(b"2\n")
                     .unwrap();
             }
-            for dividend in (3..=limit).step_by(2) {
-                let reader = BufReader::new(File::open("primes.dat").unwrap());
-                for divisor in reader.lines() {
+            for dividend in (start..=limit).step_by(2) {
+                let reader = BufReader::new(File::open(FILE).unwrap()).lines();
+                for divisor in reader {
                     let divisor = divisor.unwrap().parse::<u128>().unwrap();
-                    if divisor * divisor > dividend {
-                        BufWriter::new(OpenOptions::new().append(true).open("primes.dat").unwrap())
+                    if divisor >= SQRT_MAX || divisor * divisor > dividend {
+                        BufWriter::new(OpenOptions::new().append(true).open(FILE).unwrap())
                             .write_all(format!("{}\n", dividend).as_bytes())
                             .unwrap();
                         break;
@@ -27,9 +45,7 @@ fn main() {
                 }
             }
         }
-        Err(_) => println!(
-            "Read the README.md file to understand how to use this application and try again."
-        ),
+        Err(_) => println!("The input is not an integer."),
     }
     return;
 }
